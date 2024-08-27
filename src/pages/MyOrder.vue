755249ac423571a6ddcae72d43481b2149f173e7
Kenost <Kenostru@mail.ru>
2024-08-27 19:23:44 +0500

    upd

--- src/pages/MyOrder.vue
+++ src/pages/MyOrder.vue
@@ -24,7 +24,7 @@
 				</div>
 				<div class="w-full kenost-table-elem">
 					<span>Поставщик</span>
-					<div class="kenost-table-elem__content">{{ this.order.name_short }}</div>
+					<div class="kenost-table-elem__content"><img class="kenost-table-elem__logo" :src="this.order.seller_image">{{ this.order.name_short }}</div>
 				</div>
 				<div class="w-full kenost-table-elem">
 					<span>Отсрочка</span>
@@ -39,9 +39,50 @@
 				<div><img src="../assets/images/icons/delivery.svg" alt=""></div>
 				<span>Доставка</span>
 			</div>
+			<div class="w-full flex">
+				<div class="w-full kenost-table-elem">
+					<span>Оплата доставки</span>
+					<div class="kenost-table-elem__content"><img class="kenost-table-elem__logo" :src="this.order.seller_image">???</div>
+				</div>
+				<div class="w-full kenost-table-elem">
+					<span>Срок доставки</span>
+					<div class="kenost-table-elem__content">? дня (??.??.??)</div>
+				</div>
+				<div class="w-full kenost-table-elem">
+					<span>Склад</span>
+					<div class="kenost-table-elem__content">{{ this.order.seller_address }}</div>
+				</div>
+			</div>
 			<div class="kenost-table-line">
 				<div><img src="../assets/images/icons/orders.svg" alt=""></div>
 				<span>Состав заказа</span>
+			</div>
+			<table class="std-table">
+				<thead class="std-table__head white">
+					<tr class="std-table__row">
+						<th class="std-table__hcol">Артикул</th>
+						<th class="std-table__hcol">Фото</th>
+						<th class="std-table__hcol">Название</th>
+						<th class="std-table__hcol">Стоимость за еденицу</th>
+						<th class="std-table__hcol">Количество</th>
+						<th class="std-table__hcol">Сумма</th>
+					</tr>
+				</thead>
+				<tbody class="std-table__body">
+					<tr class="std-table__row" v-for="item in order.products" v-bind:key="item.id">
+						<td class="std-table__col">{{ item.article }}</td>
+						<td class="std-table__col">
+							<img class="kenost-image-table" :src="item.image" alt="">
+						</td>
+						<td class="std-table__col">{{ item.name }}</td>
+						<td class="std-table__col">{{ item.price }}</td>
+						<td class="std-table__col">{{ item.count }}</td>
+						<td class="std-table__col">{{ item.price * item.count }}</td>
+					</tr>
+				</tbody>
+			</table>
+			<div class="kenost-table-line right">
+				<b>Итого: 34 046 ₽ </b>
 			</div>
 		</div>
 	</div>
