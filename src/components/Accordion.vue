5b577a4c59b2f5df39c01add51174ff8f27fa282
Daniil <baldindaniilza@mail.ru>
2024-09-03 11:41:10 +0500

    fix

---
+++ src/components/Accordion.vue
@@ -0,0 +1,90 @@
+<template>
+    <div :class="{ 'std-accordion': true, 'std-accordion--expanded': this.isExpanded}">
+        <div class="std-accordion__header" @click="toggleExpanded">
+            {{ title }}
+            <button class="std-accordion__button">
+                <img src="../assets/images/icons/arrow.svg" class="std-accordion__img">
+            </button>
+        </div>
+        <div class="std-accordion__content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import { ref } from 'vue';
+
+export default {
+    setup () {
+        let isExpanded = ref(false);
+
+        return {
+            isExpanded
+        }
+    },  
+    props: {
+        title: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        toggleExpanded() {
+            this.isExpanded = !this.isExpanded;
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.std-accordion {
+    display: flex;
+    flex-direction: column;
+
+    &--expanded {
+        .std-accordion__img {
+            rotate: -90deg;
+        }
+        
+        .std-accordion__content {
+            max-height: 100dvh;
+        }
+    }    
+
+    &__header {
+        cursor: pointer;
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        gap: 15px;
+
+        width: 100%;
+
+    }
+
+    &__button {
+        cursor: pointer;
+
+        background: none;
+        border: none;
+        outline: none;
+    }
+
+    &__img {
+        transition-duration: .3s;
+        rotate: 90deg;
+    }
+
+    &__content {
+        display: flex;
+        flex-direction: column;
+
+        max-height: 0;
+        overflow: hidden;
+        transition-duration: .3s;
+    }
+}
+</style>
