84e98ada178872a6f0c3d8dcc02045d9747a46a6
Daniil <baldindaniilza@mail.ru>
2024-09-06 17:14:12 +0500

    Переделано: Смена организации Старая модалка изменена на модалку без шапки + Адаптив

--- src/components/Sitebar.vue
+++ src/components/Sitebar.vue
@@ -8,11 +8,11 @@
 			<img src="/src/assets/images/logo_small.svg" alt="МСТ Аналитика" />
 			<span>МСТ Аналитика</span>
 		</router-link>
-    <router-link v-if="role == 1" :to="{ name: 'retail_orders' }" class="sitebar-logo">
+		<router-link v-if="role == 1" :to="{ name: 'retail_orders' }" class="sitebar-logo">
 			<img src="/src/assets/images/logo_small.svg" alt="МСТ Аналитика" />
 			<span>МСТ Аналитика</span>
 		</router-link>
-    <router-link v-if="role == 2" :to="{ name: 'statistics' }" class="sitebar-logo">
+		<router-link v-if="role == 2" :to="{ name: 'statistics' }" class="sitebar-logo">
 			<img src="/src/assets/images/logo_small.svg" alt="МСТ Аналитика" />
 			<span>МСТ Аналитика</span>
 		</router-link>
@@ -22,17 +22,17 @@
 			<img src="/src/assets/images/logo_small.svg" alt="МСТ Аналитика" />
 			<span>МСТ Аналитика</span>
 		</router-link>
-    <router-link v-if="role == 1" :to="{ name: 'retail_orders' }" class="sitebar-logo">
+		<router-link v-if="role == 1" :to="{ name: 'retail_orders' }" class="sitebar-logo">
 			<img src="/src/assets/images/logo_small.svg" alt="МСТ Аналитика" />
 			<span>МСТ Аналитика</span>
 		</router-link>
-    <router-link v-if="role == 2" :to="{ name: 'statistics' }" class="sitebar-logo">
+		<router-link v-if="role == 2" :to="{ name: 'statistics' }" class="sitebar-logo">
 			<img src="/src/assets/images/logo_small.svg" alt="МСТ Аналитика" />
 			<span>МСТ Аналитика</span>
 		</router-link>
 		<div class="sitebar-toggler" @click="sidebarToggle">
 			<i class="pi pi-chevron-left"></i>
-      <!-- <img src="/images/icons/array.svg" alt=""> -->
+			<!-- <img src="/images/icons/array.svg" alt=""> -->
 		</div>
 		<div class="sitebar-profile">
 			<div class="sitebar-org">
@@ -49,7 +49,7 @@
 				<button class="sitebar-profile__tag" v-if="this.role == 1">Маркетплейс</button>
 				<button class="sitebar-profile__tag" v-if="this.role == 2">Поставщик</button>
 				<button class="sitebar-profile__on">
-          <span>Включен</span>
+					<span>Включен</span>
 				</button>
 			</div>
 		</div>
@@ -127,27 +127,45 @@
 		</router-link>
 	</Dialog> -->
 
-  <EmptyDialog :visible="this.changeOrgModal" @close="this.changeOrgModal = false">
-    <router-link
-			@click="changeOrg"
-			:to="{ name: 'org', params: { id: item.id } }"
-			class="change-org-el"
-			:class="{ active: $route.params.id == item.id }"
-			v-for="item in this.organizations"
-			v-bind:key="item.id"
-		>
-			<div class="icon"><img :src="item.image" alt="" /></div>
-			<div class="change-org-el__text">
-				<b>{{ item.name }}</b>
-				<div v-if="item.active" class="dart-payment-status-org">
+	<EmptyDialog :visible="this.changeOrgModal" @close="this.changeOrgModal = false">
+		<div class="change-org-container">
+			<div class="change-org__wrapper">
+				<div class="change-org-el" :class="{ active: true }">
+					<div class="icon"><img :src="this.activeOrganization.image" alt="" /></div>
+					<div class="change-org-el__text">
+						<b>{{ this.activeOrganization.name }}</b>
+						<p>{{ this.activeOrganization.description }}</p>
+					</div>
+				</div>
+				<div v-if="this.activeOrganization.active" class="dart-payment-status-org">
 					<i class="d_icon d_icon-check"></i><span>Включен</span>
 				</div>
 				<div v-else class="dart-payment-status-org off">
 					<i class="d_icon d_icon-focus"></i><span>Выключен</span>
 				</div>
 			</div>
-		</router-link>
-  </EmptyDialog>
+
+			<hr class="change-org__line" />
+
+			<div class="change-org__links">
+				<router-link
+					v-for="item in this.organizations.filter(
+						(org) => org.id !== this.activeOrganization.id
+					)"
+					@click="changeOrg"
+					:to="{ name: 'org', params: { id: item.id } }"
+					class="change-org-el"
+					v-bind:key="item.id"
+				>
+					<div class="icon"><img :src="item.image" alt="" /></div>
+					<div class="change-org-el__text">
+						<b>{{ item.name }}</b>
+						<p>{{ item.description }}</p>
+					</div>
+				</router-link>
+			</div>
+		</div>
+	</EmptyDialog>
 </template>
 
 <script>
@@ -159,167 +177,168 @@ import EmptyDialog from "./EmptyDialog.vue";
 import router from "../router";
 
 export default {
-  name: "Sitebar",
-  props: {
-    active: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  data() {
-    return {
-      sitebar: this.active,
-      organizations: [],
-      organozation: [],
-      changeOrgModal: false,
-      role: 0,
-      //РОЛИ
-      // 0 - Закупки
-      // 1 - Маркетплейс
-      // 2 - Поставщик
-    };
-  },
-  components: {
-    PanelMenu,
-    Dialog,
-    EmptyDialog
-  },
-  mounted() {},
-  computed: {
-    ...mapGetters({
-      getUser: "user/getUser",
-      orgs: "orgs",
-    }),
-    getMenu() {
-      if (this.role == 0) {
-        return [
-          {
-            label: "Закупки",
-            icon: "orders.svg",
-            to: { name: "purchases_home", params: { id: this.$route.params.id } },
-          },
-          {
-            label: "Мои заказы",
-            icon: "bag.svg",
-            to: { name: "my_orders", params: { id: this.$route.params.id } },
-          },
-          {
-            label: "Мои поставщики",
-            icon: "star.svg",
-            to: { name: "vendors", params: { id: this.$route.params.id } },
-          },
-          {
-            label: "Товары",
-            icon: "products.svg",
-            to: { name: "statistics", params: { id: this.$route.params.id } },
-          },
-          {
-            label: "Карточка компании",
-            icon: "settings.svg",
-            to: { name: "org_settings", params: { id: this.$route.params.id } },
-          },
-        ];
-      } else if (this.role == 1) {
-        return [
-          {
-            label: "Розничные заказы",
-            icon: "orders.svg",
-            to: { name: "retail_orders", params: { id: this.$route.params.id } },
-          },
-          {
-            label: "Баланс",
-            icon: "bag.svg",
-            to: { name: "org_balance", params: { id: this.$route.params.id } },
-          },
-          {
-            label: "Розничные акции",
-            icon: "star.svg",
-            to: { name: "b2c", params: { id: this.$route.params.id } },
-          },
-          {
-            label: "Товары",
-            icon: "products.svg",
-            to: { name: "stores", params: { id: this.$route.params.id } },
-          },
-          {
-            label: "Карточка компании",
-            icon: "settings.svg",
-            to: { name: "org_settings", params: { id: this.$route.params.id } },
-          },
-        ];
-      } else if (this.role == 2) {
-        return [
-          {
-            label: "Товары",
-            icon: "products.svg",
-            to: { name: "statistics", params: { id: this.$route.params.id } },
-          },
-          {
-            label: "Отгрузки",
-            icon: "shipments.svg",
-            to: { name: "shipments", params: { id: this.$route.params.id } },
-          },
-          {
-            label: "Мои клиенты",
-            icon: "clients.svg",
-            to: { name: "clients", params: { id: this.$route.params.id } },
-          },
-          {
-            label: "Индивидуальные скидки",
-            icon: "star.svg",
-            to: { name: "discounts", params: { id: this.$route.params.id } },
-          },
-          {
-            label: "Оптовые акции",
-            icon: "adv.svg",
-            to: { name: "b2b", params: { id: this.$route.params.id } },
-          },
-          {
-            label: "Карточка компании",
-            icon: "settings.svg",
-            to: { name: "org_settings", params: { id: this.$route.params.id } },
-          },
-        ];
-      }
-    },
-  },
-  methods: {
-    ...mapActions({
-      setUser: "user/setUser",
-      deleteUser: "user/deleteUser",
-      org_get_from_api: "org_get_from_api",
-    }),
-    sidebarToggle() {
-      this.sitebar = !this.sitebar;
-      this.$cookies.set("sidebar_active", Number(this.sitebar));
-    },
-    onAuthBtnClick() {
-      if (this.getUser) {
-        this.$api.auth.logout();
-        localStorage.removeItem("user");
-        this.deleteUser();
-        // console.log(this.$router)
-        this.$router.push({ name: "main" });
-      } else {
-        this.$router.push({ name: "main" });
-      }
-    },
-    changeRole(role) {
-      //РОЛИ
-      // 0 - Закупки
-      // 1 - Маркетплейс
-      // 2 - Поставщик
+	name: "Sitebar",
+	props: {
+		active: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	data() {
+		return {
+			activeOrganization: {},
+			sitebar: this.active,
+			organizations: [],
+			organozation: [],
+			changeOrgModal: false,
+			role: 0,
+			//РОЛИ
+			// 0 - Закупки
+			// 1 - Маркетплейс
+			// 2 - Поставщик
+		};
+	},
+	components: {
+		PanelMenu,
+		Dialog,
+		EmptyDialog,
+	},
+	mounted() {},
+	computed: {
+		...mapGetters({
+			getUser: "user/getUser",
+			orgs: "orgs",
+		}),
+		getMenu() {
+			if (this.role == 0) {
+				return [
+					{
+						label: "Закупки",
+						icon: "orders.svg",
+						to: { name: "purchases_home", params: { id: this.$route.params.id } },
+					},
+					{
+						label: "Мои заказы",
+						icon: "bag.svg",
+						to: { name: "my_orders", params: { id: this.$route.params.id } },
+					},
+					{
+						label: "Мои поставщики",
+						icon: "star.svg",
+						to: { name: "vendors", params: { id: this.$route.params.id } },
+					},
+					{
+						label: "Товары",
+						icon: "products.svg",
+						to: { name: "statistics", params: { id: this.$route.params.id } },
+					},
+					{
+						label: "Карточка компании",
+						icon: "settings.svg",
+						to: { name: "org_settings", params: { id: this.$route.params.id } },
+					},
+				];
+			} else if (this.role == 1) {
+				return [
+					{
+						label: "Розничные заказы",
+						icon: "orders.svg",
+						to: { name: "retail_orders", params: { id: this.$route.params.id } },
+					},
+					{
+						label: "Баланс",
+						icon: "bag.svg",
+						to: { name: "org_balance", params: { id: this.$route.params.id } },
+					},
+					{
+						label: "Розничные акции",
+						icon: "star.svg",
+						to: { name: "b2c", params: { id: this.$route.params.id } },
+					},
+					{
+						label: "Товары",
+						icon: "products.svg",
+						to: { name: "stores", params: { id: this.$route.params.id } },
+					},
+					{
+						label: "Карточка компании",
+						icon: "settings.svg",
+						to: { name: "org_settings", params: { id: this.$route.params.id } },
+					},
+				];
+			} else if (this.role == 2) {
+				return [
+					{
+						label: "Товары",
+						icon: "products.svg",
+						to: { name: "statistics", params: { id: this.$route.params.id } },
+					},
+					{
+						label: "Отгрузки",
+						icon: "shipments.svg",
+						to: { name: "shipments", params: { id: this.$route.params.id } },
+					},
+					{
+						label: "Мои клиенты",
+						icon: "clients.svg",
+						to: { name: "clients", params: { id: this.$route.params.id } },
+					},
+					{
+						label: "Индивидуальные скидки",
+						icon: "star.svg",
+						to: { name: "discounts", params: { id: this.$route.params.id } },
+					},
+					{
+						label: "Оптовые акции",
+						icon: "adv.svg",
+						to: { name: "b2b", params: { id: this.$route.params.id } },
+					},
+					{
+						label: "Карточка компании",
+						icon: "settings.svg",
+						to: { name: "org_settings", params: { id: this.$route.params.id } },
+					},
+				];
+			}
+		},
+	},
+	methods: {
+		...mapActions({
+			setUser: "user/setUser",
+			deleteUser: "user/deleteUser",
+			org_get_from_api: "org_get_from_api",
+		}),
+		sidebarToggle() {
+			this.sitebar = !this.sitebar;
+			this.$cookies.set("sidebar_active", Number(this.sitebar));
+		},
+		onAuthBtnClick() {
+			if (this.getUser) {
+				this.$api.auth.logout();
+				localStorage.removeItem("user");
+				this.deleteUser();
+				// console.log(this.$router)
+				this.$router.push({ name: "main" });
+			} else {
+				this.$router.push({ name: "main" });
+			}
+		},
+		changeRole(role) {
+			//РОЛИ
+			// 0 - Закупки
+			// 1 - Маркетплейс
+			// 2 - Поставщик
 
 			localStorage.setItem("role", role);
 			this.role = role;
 
-      if(role == 0){
-        this.$router.push({ name: "purchases_home" });
-      }else if(role == 1){
-        this.$router.push({ name: "retail_orders" });
-      }else if(role == 2){
-        this.$router.push({ name: "statistics" });
-      }
+			if (role == 0) {
+				this.$router.push({ name: "purchases_home" });
+			} else if (role == 1) {
+				this.$router.push({ name: "retail_orders" });
+			} else if (role == 2) {
+				this.$router.push({ name: "statistics" });
+			}
 		},
 		getRole() {
 			if (localStorage.getItem("role")) {
@@ -350,14 +369,16 @@ export default {
 	watch: {
 		orgs: function (newVal, oldVal) {
 			this.organizations = newVal;
-			// console.log(newVal)
 			if (newVal) {
 				const org = newVal.find((el) => el.id === this.$route.params.id);
-				// console.log(org)
 				if (org) {
 					this.organozation = org;
 				}
 			}
+
+			this.activeOrganization = this.organizations.find(
+				(org) => org.id === this.$route.params.id
+			);
 		},
 	},
 };
