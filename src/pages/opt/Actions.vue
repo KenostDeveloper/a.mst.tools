5a56e2f2d753d16333e54fcc9c74c105453a6d1e
Kenost <Kenostru@mail.ru>
2024-09-10 20:08:58 +0500

    upd

--- src/pages/opt/Actions.vue
+++ src/pages/opt/Actions.vue
@@ -4,15 +4,15 @@
         <div class="dart-row">
             <router-link :to="{ name: 'promotion', params: { id: this.$route.params.id, action: item.id } }" v-for="item in action_buyer.items" v-bind:key="item.id" class="d-col-lg-4 d-col-sm-6 d-col-12 link-no-style">
                 <div class="kenost-action-opt">
-                    <img class="kenost-action-opt__image" src="https://mst.tools/assets/images/templates/action-imge-base.png" alt="">
-                    <!-- <img class="kenost-action-opt__image" :src="item.image" alt=""> -->
+                    <!-- <img class="kenost-action-opt__image" src="https://mst.tools/assets/images/templates/action-imge-base.png" alt=""> -->
+                    <img class="kenost-action-opt__image" :src="item.image" alt="">
                     <div class="kenost-action-opt__name">{{ item.action_name }}</div>
                     <!-- {{ item }} -->
                     <div class="kenost-action-opt__params">
                         <div class="kenost-action-opt__param"><span>Действует до:</span>{{ new Date(item.date_to).toLocaleString("ru", {month: '2-digit', day: '2-digit', year: 'numeric'}) }}</div>
                         <div class="kenost-action-opt__param"><span>Склад:</span>{{ item.store_city }}</div>
-                        <div class="kenost-action-opt__param"><span>Поставщик:</span><img :src="item.store_image" alt="">{{ item.name_short }}</div>
                     </div>
+                    <div class="kenost-action-opt__param mt-2"><span>Поставщик:</span><img :src="item.store_image" alt=""><div class="kenost-action-opt__name-store">{{ item.name_short }}</div></div>
                 </div>
             </router-link>
         </div>
