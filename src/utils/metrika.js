13aef55ed77dbf37cde8d5588fbb462f9fddf5e5
Petropavlovskiy Artiom <artpetropavlovskij@gmail.com>
2024-09-30 17:25:12 +0500

    Фикс багов

--- src/utils/metrika.js
+++ src/utils/metrika.js
@@ -1,15 +1,11 @@
 import { YM_ID } from '../constants/metrika'
 
 export const sendMetrik = (action) => {
-  console.log(process.env.NODE_ENV)
-  // console.log(window.ym)
-  // console.log(YM_ID)
   if (typeof window !== 'undefined' && typeof window.ym !== 'undefined'){
     if(process.env.NODE_ENV === 'production'){
       window.ym(YM_ID, 'reachGoal', action);
     }else{
       console.log('sendMetrik', action);
-      // window.ym(YM_ID, 'reachGoal', action);
     }
   }
 }
