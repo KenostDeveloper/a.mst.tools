208a53d3f85f6118beb1f8160f8203ab2e4025a4
Daniil <baldindaniilza@mail.ru>
2024-10-12 13:47:44 +0500

    Update: Custom toast Стили для кастомного уведомления Toast

--- src/loyouts/MainLoyout.vue
+++ src/loyouts/MainLoyout.vue
@@ -1,11 +1,24 @@
 <template>
     <Toast>
-
         <template #message="slotProps">
-            <div v-if="slotProps.message.severity === 'secondary'" class="custom-toast">
-                <h3>{{ slotProps.message.summary }}</h3>
-                <p>{{ slotProps.message.detail }}</p>
-                <button @click="handleClick">Дополнительное действие</button>
+            <div v-if="slotProps.message.severity === 'secondary'" class="std-notification">
+                <div class="custom-toast">
+                    <div class="std-notification__header">
+                        <div class="std-notification__header-content">
+                            <i class="std_icon std_icon-notification std-notification__icon"></i>
+                            <span class="std-notification__span">12.10.2024</span>
+                            <span class="std-notification__span">12:00</span>
+                        </div>
+                    </div>
+                    <div class="std-notification__main">
+                        <h6 class="std-notification__title">{{ slotProps.message.summary }}</h6>
+                        <p class="std-notification__text">{{ slotProps.message.detail }}</p>
+                        <span class="std-notification__span">
+                            Просмотреть детали заказа, нажав
+                            <router-link class="std-notification__link" to="/" @click.stop>здесь</router-link>
+                        </span>
+                    </div>
+                </div>
             </div>
 
             <div v-else>
@@ -15,8 +28,9 @@
                     <div class="p-toast-detail" data-pc-section="detail">{{ slotProps.message.detail }}</div>
                 </div>
             </div>
-      </template>
+        </template>
     </Toast>
+
     <Sitebar v-if="this.getUser" :active="this.sitebar" />
     <div class="content" :class="{ white: this.namePathIsNav == 'purchases' }">
         <div class="dart_container_wrap">
@@ -30,7 +44,6 @@
                         :lifetime="10000"
                         @delete="deleteNotification(index)" /> -->
                 <!-- </div> -->
-                
 
                 <router-link
                     v-if="this.$route.params.id"
@@ -81,20 +94,28 @@ export default {
 
         this.namePathIsNav = router?.currentRoute?._value.matched[4]?.name;
 
-        if(this.$route.params.id){
+        if (this.$route.params.id) {
             this.get_notification_api({
-                action: "get",
-                id: this.$route.params.id,
-            })
+                action: 'get',
+                id: this.$route.params.id
+            });
         }
 
-        if(this.$route.params.id){
+        if (this.$route.params.id) {
             this.intervalId = setInterval(() => {
                 this.fetchNotification();
             }, 40000);
 
             this.fetchNotification();
         }
+
+        this.$toast.add({
+            severity: 'secondary',
+            summary: 'Добро пожаловать!',
+            detail: 'Вы вошли в панель управления',
+            life: 1000000,
+            styleClass: 'std-toast'
+        });
     },
     updated() {
         // this.setUser(JSON.parse(localStorage.getItem('user')))
@@ -105,71 +126,71 @@ export default {
             setUser: 'user/setUser',
             deleteUser: 'user/deleteUser',
             get_new_notification_api: 'get_new_notification_api',
-            get_notification_api: 'get_notification_api',
+            get_notification_api: 'get_notification_api'
         }),
         deleteNotification(index) {
             this.notifications.splice(index, 1);
         },
         fetchNotification() {
             this.get_new_notification_api({
-                action: "get",
+                action: 'get',
                 id: this.$route.params.id,
                 data_start: this.data_start
             }).then((res) => {
-                for(let i = 0; i < res.data.data?.total; i++){
+                for (let i = 0; i < res.data.data?.total; i++) {
                     setTimeout(() => {
-                        let title = "";
-                        switch(res.data.data.items[i].namespace){
+                        let title = '';
+                        switch (res.data.data.items[i].namespace) {
                             case '1':
-                                title = "Изменение статуса заказа в маркетплейсе"
+                                title = 'Изменение статуса заказа в маркетплейсе';
                                 break;
                             case '2':
-                                title = "Поступил новый оптовый заказ"
+                                title = 'Поступил новый оптовый заказ';
                                 break;
                             case '3':
-                                title = "Ваша компания отключена"
+                                title = 'Ваша компания отключена';
                                 break;
                             case '4':
-                                title = "Ваша компания подключена"
+                                title = 'Ваша компания подключена';
                                 break;
                             case '5':
-                                title = "Появился новый поставщик"
+                                title = 'Появился новый поставщик';
                                 break;
                             case '6':
-                                title = "Появился новый поставщик"
+                                title = 'Появился новый поставщик';
                                 break;
                             case '7':
-                                title = "Вас добавили в поставщики"
+                                title = 'Вас добавили в поставщики';
                                 break;
                             case '8':
-                                title = "Вас удалили из поставщиков"
+                                title = 'Вас удалили из поставщиков';
                                 break;
                             case '9':
-                                title = "Ваш склад отключен"
+                                title = 'Ваш склад отключен';
                                 break;
                             case '10':
-                                title = "Ваш склад подключен"
+                                title = 'Ваш склад подключен';
                                 break;
                         }
-                        console.log(title)
+                        console.log(title);
                         this.$toast.add({ severity: 'secondary', summary: title, detail: 'Нажмите, чтобы узнать подробнее', life: 5000 });
-                    }, i * 500)
+                    }, i * 500);
                 }
-                if(res.data.data.items.length > 0){
-                    if(this.$route.params.id){
+                if (res.data.data.items.length > 0) {
+                    if (this.$route.params.id) {
                         this.get_notification_api({
-                            action: "get",
-                            id: this.$route.params.id,
-                        })
+                            action: 'get',
+                            id: this.$route.params.id
+                        });
                     }
                 }
-            })
+            });
             this.data_start = new Date();
-        },
+        }
     },
     watch: {
         notifications: function (newVal, oldVal) {
-            this.notifications_all = newVal
+            this.notifications_all = newVal;
         }
     }
 };
