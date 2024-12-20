06201e5b4cf132eddc63f9029f7a0029ebfe8ef3
Kenost <Kenostru@mail.ru>
2024-12-20 19:54:40 +0500

    Фикс модального окна акций

--- src/components/opt/ActionModal.vue
+++ src/components/opt/ActionModal.vue
@@ -3,12 +3,12 @@
         <div class="table-actions__modal" :class="{'pt-3': (!action?.image.image || action?.type == 3 || !action?.image.thumb_medium) && !action.image}">
             <img v-if="action?.image.image && action?.type != 3 && action?.image.thumb_medium" :src="action?.image.thumb_medium"
                 :srcset="action?.image.image +' 2x, ' + action?.image.thumb_medium + ' 1x'" :alt="action?.name" class="table-actions__modal-img">
-            <img v-else-if="action.image && action?.type != 3" :src="site_url_prefix + 'assets/content/' + action.image" :alt="action?.name" class="table-actions__modal-img">
+            <img v-else-if="action.image && action?.type != 3" :src="action.image" :alt="action?.name" class="table-actions__modal-img">
 
             <p class="table-actions__modal-title mt-3">{{ action?.type == 3 ? "Индивидуальная акция" : action?.name }}</p>
 
             <div class="kenost-action-modal">
-                <div class="kenost-action-modal__item" :class="{'none': tag?.type != 'min'}" v-for="(tag, index) in action.tags.slice(0, !(action.tags.length % 2)?  action.tags.length - Math.floor(action.tags.length / 2) + 1:action.tags.length - Math.floor(action.tags.length / 2))" v-bind:key="tag.id">
+                <div class="kenost-action-modal__item" :class="{'none': tag?.type == 'min'}" v-for="(tag, index) in action.tags.slice(0, !(action.tags.length % 2)?  action.tags.length - Math.floor(action.tags.length / 2) + 1:action.tags.length - Math.floor(action.tags.length / 2))" v-bind:key="tag.id">
                     <img v-if="tag.type == 'min_sum'"
                         src="../../assets/images/icons/action/basket.svg" alt="" />
                     <p v-if="tag.type == 'min_sum'">
